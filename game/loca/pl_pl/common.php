eacd65becdde6074dd4af4ea52e47ad452972df4
Kamil Kosiński <kamkos96@users.noreply.github.com>
2026-01-02 03:52:10 +0100

    Add Polish localization for common texts

---
+++ game/loca/pl_pl/common.php
@@ -0,0 +1,47 @@
+<?php
+
+// Wspólne teksty. Występują na większości stron.
+
+$LOCA["pl"]["PAGE_TITLE"] = "Uni#1 OGame";
+
+$LOCA["pl"]["METAL" ] = "Metal";
+$LOCA["pl"]["CRYSTAL" ] = "Kryształ";
+$LOCA["pl"]["DEUTERIUM" ] = "Deuter";
+$LOCA["pl"]["ENERGY" ] = "Energia";
+$LOCA["pl"]["DM" ] = "Ciemna Materia";
+
+$LOCA["pl"]["PLANET_HOME"] = "Planeta Matka";
+$LOCA["pl"]["PLANET_COLONY"] = "Kolonia";
+$LOCA["pl"]["PLANET_PHANTOM"] = "Planeta";
+$LOCA["pl"]["PLANET_ABANDONED"] = "Opuszczona planeta";
+$LOCA["pl"]["PLANET_DESTROYED"] = "Zniszczona planeta";
+$LOCA["pl"]["MOON"] = "Księżyc";
+$LOCA["pl"]["MOON_DESTROYED"] = "Opuszczony";
+$LOCA["pl"]["DEBRIS"] = "Pole Szczątków";
+$LOCA["pl"]["FAR_SPACE"] = "Głęboka przestrzeń";
+
+$LOCA["pl"]["TIME_DAYS"] = "d";
+$LOCA["pl"]["TIME_HOUR"] = "g";
+$LOCA["pl"]["TIME_MIN"] = "m";
+$LOCA["pl"]["TIME_SEC"] = "s";
+
+// Dowódca i oficerowie.
+
+$LOCA["pl"]["PR_COMA" ] = "Dowódca OGame";
+$LOCA["pl"]["PR_ADMIRAL" ] = "Admirał Floty";
+$LOCA["pl"]["PR_ENGINEER" ] = "Inżynier";
+$LOCA["pl"]["PR_GEOLOGIST" ] = "Geolog";
+$LOCA["pl"]["PR_TECHNO" ] = "Technokrata";
+
+$LOCA["pl"]["PR_ADMIRAL_INFO" ] = "&amp;nbsp;maks. liczba flot +2";
+$LOCA["pl"]["PR_ENGINEER_INFO" ] = "Zmniejsza straty w obronie o połowę&lt;br&gt; +10% więcej energii";
+$LOCA["pl"]["PR_GEOLOGIST_INFO" ] = "+10% produkcja kopalń";
+$LOCA["pl"]["PR_TECHNO_INFO" ] = "+2 poziom szpiegostwa dla sond, 25% krótszy czas badań";
+
+$LOCA["pl"]["PR_PURCHASE" ] = "Zamów teraz!";
+$LOCA["pl"]["PR_RENEW" ] = "Odnów!";
+$LOCA["pl"]["PR_ACTIVE_DAYS" ] = "&lt;font color=lime&gt;Aktywny&lt;/font&gt; jeszcze przez #1 dni";
+
+$LOCA["pl"]["GLOBAL_ERROR"] = "Wystąpił błąd";
+
+?>
