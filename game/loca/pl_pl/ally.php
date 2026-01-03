a5162e3c021b5d907b09b780577cbe17091b0083
Kamil Kosiński <kamkos96@users.noreply.github.com>
2026-01-03 02:45:57 +0100

    Add Polish localization for alliance system texts

---
+++ game/loca/pl_pl/ally.php
@@ -0,0 +1,231 @@
+<?php
+
+// System sojuszowy
+
+// Ogólne
+
+$LOCA["pl"]["ALLY_ALLY"] = "Sojusz";
+$LOCA["pl"]["ALLY_NO_WAY"] = "Niewystarczające uprawnienia do wykonania operacji";
+$LOCA["pl"]["ALLY_BACK"] = "Powrót do przeglądu sojuszu";
+
+// Tworzenie i szukanie sojuszy
+
+$LOCA["pl"]["ALLY_FOUND_OWN"] = "Załóż własny sojusz";
+$LOCA["pl"]["ALLY_FIND_OTHER"] = "Wyszukaj sojusze";
+
+$LOCA["pl"]["ALLY_FOUND_ALLY"] = "Załóż sojusz";
+$LOCA["pl"]["ALLY_FOUND_TAG"] = "Tag sojuszu (3-8 znaków)";
+$LOCA["pl"]["ALLY_FOUND_NAME"] = "Nazwa sojuszu (3-30 znaków)";
+$LOCA["pl"]["ALLY_FOUND_SUBMIT"] = "Załóż";
+$LOCA["pl"]["ALLY_FOUND_ERROR_TAG"] = "Tag sojuszu jest zbyt krótki";
+$LOCA["pl"]["ALLY_FOUND_ERROR_NAME"] = "Nazwa sojuszu jest zbyt krótka";
+$LOCA["pl"]["ALLY_FOUND_ERROR_EXISTS"] = "Sojusz #1 niestety już istnieje!";
+$LOCA["pl"]["ALLY_FOUND_SUCCESS"] = "Sojusz #1 [#2] został pomyślnie utworzony";
+$LOCA["pl"]["ALLY_FOUND_CONFIRM"] = "Tak!";
+
+$LOCA["pl"]["ALLY_FIND_ALLY"] = "Wyszukaj sojusze";
+$LOCA["pl"]["ALLY_FIND_HEAD"] = "Nazwa sojuszu"; // Head
+$LOCA["pl"]["ALLY_FIND_SUBMIT"] = "Szukaj"; // Submit button
+$LOCA["pl"]["ALLY_FIND_RESULT"] = "Wyniki wyszukiwania sojuszy";
+$LOCA["pl"]["ALLY_FIND_TAG"] = "Tag sojuszu";
+$LOCA["pl"]["ALLY_FIND_NAME"] = "Nazwa sojuszu";
+$LOCA["pl"]["ALLY_FIND_MEMBERS"] = "Liczba członków";
+
+$LOCA["pl"]["ALLY_APPLY"] = "Twoje zgłoszenie";
+$LOCA["pl"]["ALLY_APPLY_ALREADY"] = "Już zgłosiłeś się do sojuszu [#1]. Poczekaj na odpowiedź lub wycofaj swoje zgłoszenie.";
+$LOCA["pl"]["ALLY_APPLY_WITHDRAW"] = "Wycofaj zgłoszenie";
+
+$LOCA["pl"]["ALLY_NEW_DEFAULT_TEXT"] = "Witaj na stronie sojuszu";
+$LOCA["pl"]["ALLY_NEW_RANK_FOUNDER"] = "Założyciel";
+$LOCA["pl"]["ALLY_NEW_RANK_NEWCOMER"] = "Nowicjusz";
+
+// ranks
+
+$LOCA["pl"]["ALLY_RANK_ERROR_SPECIAL_CHARS"] = "Ranga zawiera znaki specjalne";
+$LOCA["pl"]["ALLY_RANK_MASK"] = "Nadaj uprawnienia";
+$LOCA["pl"]["ALLY_RANK_NAME"] = "Nazwa rangi";
+$LOCA["pl"]["ALLY_RANK_DELETE"] = "Usuń rangę";
+$LOCA["pl"]["ALLY_RANK_SAVE"] = "Zapisz";
+$LOCA["pl"]["ALLY_RANK_ADD_TEXT"] = "Utwórz nową rangę";
+$LOCA["pl"]["ALLY_RANK_ADD_SUBMIT"] = "Utwórz";
+$LOCA["pl"]["ALLY_RANK_INFO"] = "Wyjaśnienie uprawnień";
+
+$LOCA["pl"]["ALLY_RANK_1"] = "Rozwiązywanie sojuszu";
+$LOCA["pl"]["ALLY_RANK_2"] = "Wyrzucanie graczy";
+$LOCA["pl"]["ALLY_RANK_3"] = "Przeglądanie zgłoszeń";
+$LOCA["pl"]["ALLY_RANK_4"] = "Przeglądanie listy członków";
+$LOCA["pl"]["ALLY_RANK_5"] = "Edycja zgłoszeń";
+$LOCA["pl"]["ALLY_RANK_6"] = "Zarządzanie sojuszem";
+$LOCA["pl"]["ALLY_RANK_7"] = "Widok statusu \"online\" na liście członków";
+$LOCA["pl"]["ALLY_RANK_8"] = "Wysyłanie komunikatów";
+$LOCA["pl"]["ALLY_RANK_9"] = "'Prawa ręka' (potrzebne do przekazania statusu założyciela)";
+
+// main
+
+$LOCA["pl"]["ALLY_MAIN_HEAD"] = "Twój sojusz";
+$LOCA["pl"]["ALLY_MAIN_TAG"] = "Tag";
+$LOCA["pl"]["ALLY_MAIN_NAME"] = "Nazwa";
+$LOCA["pl"]["ALLY_MAIN_PREV"] = "b. #1";
+$LOCA["pl"]["ALLY_MAIN_MEMBERS"] = "Członkowie";
+$LOCA["pl"]["ALLY_MAIN_MEMBERS_LINK"] = "lista członków";
+$LOCA["pl"]["ALLY_MAIN_RANK"] = "Twoja ranga";
+$LOCA["pl"]["ALLY_MAIN_SETTINGS_LINK"] = "Zarządzanie sojuszem";
+$LOCA["pl"]["ALLY_MAIN_APPS"] = "Zgłoszenia";
+$LOCA["pl"]["ALLY_MAIN_APP_COUNT"] = "Ilość zgłoszeń: #1";
+$LOCA["pl"]["ALLY_MAIN_CIRCULAR"] = "Komunikat";
+$LOCA["pl"]["ALLY_MAIN_CIRCULAR_LINK"] = "Wyślij komunikat";
+$LOCA["pl"]["ALLY_MAIN_HOMEPAGE"] = "Strona domowa";
+$LOCA["pl"]["ALLY_MAIN_INTTEXT"] = "Informacje dla członków sojuszu";
+$LOCA["pl"]["ALLY_MAIN_LEAVE"] = "Opuść ten sojusz";
+$LOCA["pl"]["ALLY_MAIN_LEAVE_SUBMIT"] = "Opuść";
+
+// circular
+
+$LOCA["pl"]["ALLY_CIRC_USERLIST"] = "Następujący gracze otrzymali Twój komunikat";
+$LOCA["pl"]["ALLY_CIRC_SUBMIT"] = "Ok";
+$LOCA["pl"]["ALLY_CIRC_ERROR"] = "Błąd";
+$LOCA["pl"]["ALLY_CIRC_ERROR_TEXT"] = "Niestety, nie znaleziono odbiorców.";
+$LOCA["pl"]["ALLY_CIRC_BACK"] = "Wstecz";
+$LOCA["pl"]["ALLY_CIRC_HEAD"] = "Wyślij komunikat";
+$LOCA["pl"]["ALLY_CIRC_TO"] = "Odbiorcy";
+$LOCA["pl"]["ALLY_CIRC_ALL"] = "Wszyscy gracze";
+$LOCA["pl"]["ALLY_CIRC_RANK"] = "Gracze o randze: #1";
+$LOCA["pl"]["ALLY_CIRC_MESSAGE"] = "Tekst komunikatu (#1 / #2 znaków)";
+$LOCA["pl"]["ALLY_CIRC_SEND"] = "Wyślij";
+
+// settings
+
+$LOCA["pl"]["ALLY_SETTINGS_HEAD"] = "Zarządzanie sojuszem";
+$LOCA["pl"]["ALLY_SETTINGS_RANKS"] = "Ustaw rangi";
+$LOCA["pl"]["ALLY_SETTINGS_MEMBERS"] = "Członkowie sojuszu";
+$LOCA["pl"]["ALLY_SETTINGS_CHANGE_TAG"] = "Zmień tag sojuszu (tylko raz w tygodniu)";
+$LOCA["pl"]["ALLY_SETTINGS_CHANGE_NAME"] = "Zmień nazwę sojuszu (tylko raz w tygodniu)";
+$LOCA["pl"]["ALLY_SETTINGS_EDIT"] = "Edytuj tekst";
+$LOCA["pl"]["ALLY_SETTINGS_EXTTEXT"] = "Publiczny";
+$LOCA["pl"]["ALLY_SETTINGS_INTTEXT"] = "Dla członków";
+$LOCA["pl"]["ALLY_SETTINGS_APPTEXT"] = "Szablon zgłoszenia";
+$LOCA["pl"]["ALLY_SETTINGS_EXTTEXT_DEF"] = "Publiczny opis sojuszu";
+$LOCA["pl"]["ALLY_SETTINGS_INTTEXT_DEF"] = "Informacje dla członków sojuszu";
+$LOCA["pl"]["ALLY_SETTINGS_APPTEXT_DEF"] = "Szablon zgłoszenia do sojuszu";
+$LOCA["pl"]["ALLY_SETTINGS_CHARS"] = "#1 znaków";
+$LOCA["pl"]["ALLY_SETTINGS_APP_SAMPLE"] = "Użyj szablonu zgłoszenia";
+$LOCA["pl"]["ALLY_SETTINGS_HIDE_APP"] = "ręcznie";
+$LOCA["pl"]["ALLY_SETTINGS_SHOW_APP"] = "automatycznie";
+$LOCA["pl"]["ALLY_SETTINGS_DELETE"] = "Usuń";
+$LOCA["pl"]["ALLY_SETTINGS_SAVE"] = "Zapisz";
+$LOCA["pl"]["ALLY_SETTINGS_TITLE"] = "Ustawienia";
+$LOCA["pl"]["ALLY_SETTINGS_HOMEPAGE"] = "Strona domowa";
+$LOCA["pl"]["ALLY_SETTINGS_LOGO"] = "Logo sojuszu (URL)";
+$LOCA["pl"]["ALLY_SETTINGS_APPS"] = "Zgłoszenia";
+$LOCA["pl"]["ALLY_SETTINGS_APPS_OPEN"] = "możliwe (sojusz otwarty)";
+$LOCA["pl"]["ALLY_SETTINGS_APPS_CLOSED"] = "niemożliwe (sojusz zamknięty)";
+$LOCA["pl"]["ALLY_SETTINGS_FOUNDER"] = "Ranga założyciela";
+$LOCA["pl"]["ALLY_SETTINGS_DISMISS"] = "Rozwiąż sojusz";
+$LOCA["pl"]["ALLY_SETTINGS_NEXT"] = "Dalej";
+$LOCA["pl"]["ALLY_SETTINGS_LEAVE"] = "Przekaż przywództwo nad sojuszem";
+
+// members
+
+$LOCA["pl"]["ALLY_MEMBERS_DENIED"] = "Przeglądanie niemożliwe";
+$LOCA["pl"]["ALLY_MEMBERS_COUNT"] = "Lista członków (liczba: #1)";
+$LOCA["pl"]["ALLY_MEMBERS_NAME"] = "Nazwa";
+$LOCA["pl"]["ALLY_MEMBERS_STATUS"] = "Ranga";
+$LOCA["pl"]["ALLY_MEMBERS_POINTS"] = "Punkty";
+$LOCA["pl"]["ALLY_MEMBERS_COORD"] = "Współrzędne";
+$LOCA["pl"]["ALLY_MEMBERS_JOINDATE"] = "Data wstąpienia";
+$LOCA["pl"]["ALLY_MEMBERS_WRITE_MESSAGE"] = "Napisz wiadomość";
+$LOCA["pl"]["ALLY_MEMBERS_YES"] = "Tak";
+$LOCA["pl"]["ALLY_MEMBERS_NO"] = "Nie";
+$LOCA["pl"]["ALLY_MEMBERS_INACTIVE"] = "Nieaktywny";
+$LOCA["pl"]["ALLY_MEMBERS_ACTION"] = "Akcje";
+$LOCA["pl"]["ALLY_MEMBERS_KICK"] = "Wyrzuć gracza";
+$LOCA["pl"]["ALLY_MEMBERS_KICK_CONFIRM"] = "Jesteś pewien, że gracz #1 powinien opuścić sojusz?";
+$LOCA["pl"]["ALLY_MEMBERS_SET_RANK"] = "Przypisz rangę";
+$LOCA["pl"]["ALLY_MEMBERS_RANK_TO"] = "Ranga dla #1:";
+$LOCA["pl"]["ALLY_MEMBERS_SAVE"] = "Zapisz";
+$LOCA["pl"]["ALLY_MEMBERS_ID"] = "#";
+
+// misc
+
+$LOCA["pl"]["ALLY_MISC_LEAVE_CONFIRM"] = "Czy na pewno chcesz opuścić sojusz [#1]?";
+$LOCA["pl"]["ALLY_MISC_CHANGE_WAIT"] = "Poczekaj do #1";
+$LOCA["pl"]["ALLY_MISC_CHANGE_TAG_SHORT"] = "Tag sojuszu jest zbyt krótki";
+$LOCA["pl"]["ALLY_MISC_CHANGE_TAG_EXISTS"] = "Sojusz #1 niestety już istnieje!";
+$LOCA["pl"]["ALLY_MISC_CONFIRM"] = "Potwierdź";
+$LOCA["pl"]["ALLY_MISC_CHANGE_TAG_SUCCESS"] = "Sojusz o tagu [#1] ma teraz tag [#2]";
+$LOCA["pl"]["ALLY_MISC_CHANGE_OK"] = "Ok";
+$LOCA["pl"]["ALLY_MISC_CHANGE_TAG_HEAD"] = "Jak należy zmienić tag sojuszu [#1]?";
+$LOCA["pl"]["ALLY_MISC_CHANGE_TAG_NEW"] = "Nowy tag:";
+$LOCA["pl"]["ALLY_MISC_CHANGE_RENAME"] = "Zmień nazwę";
+$LOCA["pl"]["ALLY_MISC_CHANGE_NAME_SHORT"] = "Nazwa sojuszu jest zbyt krótka";
+$LOCA["pl"]["ALLY_MISC_CHANGE_NAME_SUCCESS"] = "Sojusz \"#1\" został przemianowany na \"#2\"";
+$LOCA["pl"]["ALLY_MISC_CHANGE_NAME_HEAD"] = "Jak należy zmienić nazwę sojuszu \"#1\"?";
+$LOCA["pl"]["ALLY_MISC_CHANGE_NAME_NEW"] = "Nowa nazwa:";
+
+$LOCA["pl"]["ALLY_MISC_DISMISS_SUCCESS"] = "Sojusz został rozwiązany.";
+$LOCA["pl"]["ALLY_MISC_DISMISS_OK"] = "Ok";
+$LOCA["pl"]["ALLY_MISC_DISMISS_CONFIRM"] = "Czy na pewno chcesz rozwiązać sojusz \"#1\"?";
+$LOCA["pl"]["ALLY_MISC_DISMISS_WARNING1"] = "Uwaga!";
+$LOCA["pl"]["ALLY_MISC_DISMISS_WARNING2"] = "Przywrócenie sojuszu będzie niemożliwe<br>\ni wszyscy jego członkowie go opuszczą!";
+$LOCA["pl"]["ALLY_MISC_YES_FOR_SURE"] = "Tak, chcę!";
+
+$LOCA["pl"]["ALLY_MISC_TAKEOVER_SUCCESS"] = "Ranga założyciela została przekazana";
+$LOCA["pl"]["ALLY_MISC_TAKEOVER_TAKEN"] = "Ranga założyciela została Ci przekazana";
+$LOCA["pl"]["ALLY_MISC_TAKEOVER_STILL_ACTIVE"] = "Nie można przejąć - założyciel jest aktywny";
+$LOCA["pl"]["ALLY_MISC_TAKEOVER_BACK"] = "Wstecz";
+$LOCA["pl"]["ALLY_MISC_TAKEOVER_OK"] = "OK";
+$LOCA["pl"]["ALLY_MISC_TAKEOVER_HEAD"] = "Przekaż przywództwo nad sojuszem \"#1\"";
+$LOCA["pl"]["ALLY_MISC_TAKEOVER_WHO"] = "Wybierz członka";
+$LOCA["pl"]["ALLY_MISC_TAKEOVER_RANK"] = "Ranga: #1";
+$LOCA["pl"]["ALLY_MISC_TAKEOVER_SUBMIT"] = "Przekaż";
+
+// Wiadomości sojuszu
+
+$LOCA["pl"]["ALLY_MSG_FROM"] = "Sojusz [#1]";
+$LOCA["pl"]["ALLY_MSG_CIRC_SUBJ"] = "Komunikat do członków sojuszu";
+$LOCA["pl"]["ALLY_MSG_CIRC_TEXT"] = "Gracz #1 informuje:<br>#2";
+$LOCA["pl"]["ALLY_MSG_COMMON"] = "Komunikat sojuszu";
+$LOCA["pl"]["ALLY_MSG_KICK_TEXT"] = "Gracz #1 został wykluczony z sojuszu.";
+$LOCA["pl"]["ALLY_MSG_KICK_SUBJ"] = "Wykluczenie z sojuszu";
+$LOCA["pl"]["ALLY_MSG_YOU_KICKED"] = "Gracz #1 wykluczył Cię z sojuszu [#2].<br>Możesz teraz dołączyć do innego sojuszu lub utworzyć własny.";
+$LOCA["pl"]["ALLY_MSG_LEAVE"] = "Gracz #1 opuścił sojusz.";
+$LOCA["pl"]["ALLY_MSG_DISMISS_SUBJ"] = "Rozwiązanie sojuszu";
+$LOCA["pl"]["ALLY_MSG_DISMISS"] = "Gracz #1 rozwiązał sojusz [#2].<br>Możesz teraz dołączyć do innego sojuszu lub utworzyć własny.";
+$LOCA["pl"]["ALLY_MSG_TAKEOVER_SUBJ"] = "Zmiana przywództwa w sojuszu";
+$LOCA["pl"]["ALLY_MSG_TAKEOVER"] = "Gracz #1 (założyciel) przekazał władzę nad sojuszem.";
+$LOCA["pl"]["ALLY_MSG_APPLY_ALLY"] = "Gracz #1 został przyjęty do sojuszu.";
+$LOCA["pl"]["ALLY_MSG_APPLY_YES"] = "Zgłoszenie przyjęte";
+$LOCA["pl"]["ALLY_MSG_APPLY_PLAYER"] = "Od teraz jesteś członkiem sojuszu [#1].";
+$LOCA["pl"]["ALLY_MSG_APPLY_NO"] = "Zgłoszenie odrzucone";
+$LOCA["pl"]["ALLY_MSG_APPLY_NO_REASON"] = "-przyczyna nie została podana-";
+
+// aplikowanie (zgłoszenie użytkownika)
+
+$LOCA["pl"]["ALLY_APPU_NOT_ACTIVATED"] = "Ta funkcja jest możliwa dopiero po aktywacji konta gracza.";
+$LOCA["pl"]["ALLY_APPU_TEMPLATE_MISSING"] = "Zarząd sojuszu nie udostępnił szablonu zgłoszenia.";
+$LOCA["pl"]["ALLY_APPU_TEMPLATE"] = "Wczytaj szablon";
+$LOCA["pl"]["ALLY_APPU_SUBMIT"] = "Wyślij";
+$LOCA["pl"]["ALLY_APPU_REG"] = "Zgłoszenie do sojuszu";
+$LOCA["pl"]["ALLY_APPU_SUBMITTED"] = "Twoje zgłoszenie zostało wysłane. Otrzymasz odpowiedź w przypadku przyjęcia lub odrzucenia.";
+$LOCA["pl"]["ALLY_APPU_TITLE"] = "Napisz zgłoszenie do sojuszu [#1]";
+$LOCA["pl"]["ALLY_APPU_TEXT"] = "Wiadomość (#1 / #2 znaków)";
+$LOCA["pl"]["ALLY_APPU_HINT"] = "Szablon zgłoszenia";
+$LOCA["pl"]["ALLY_APPU_FORBIDDEN"] = "Zgłaszanie się do sojuszu [#1] jest niemożliwe";
+$LOCA["pl"]["ALLY_APPU_CLOSED"] = "Ten sojusz nie przyjmuje obecnie nowych członków.";
+$LOCA["pl"]["ALLY_APPU_BACK"] = "Wstecz";
+$LOCA["pl"]["ALLY_APPU_OK"] = "OK";
+
+// zgłoszenia (panel sojuszu)
+
+$LOCA["pl"]["ALLY_APPA_ACCEPT"] = "Przyjmij";
+$LOCA["pl"]["ALLY_APPA_REJECT"] = "Odrzuć";
+$LOCA["pl"]["ALLY_APPA_FROM"] = "Zgłoszenie od #1";
+$LOCA["pl"]["ALLY_APPA_ACTION"] = "Odpowiedź na to zgłoszenie";
+$LOCA["pl"]["ALLY_APPA_REASON"] = "Przyczyna (opcjonalna) #1 / #2 znaków";
+$LOCA["pl"]["ALLY_APPA_AVAILABLE"] = "Ilość oczekujących zgłoszeń: #1<br/>Kliknij na nazwę gracza, aby obejrzeć jego zgłoszenie.";
+$LOCA["pl"]["ALLY_APPA_USER"] = "Kandydat";
+$LOCA["pl"]["ALLY_APPA_DATE"] = "Data zgłoszenia";
+$LOCA["pl"]["ALLY_APPA_OVERVIEW"] = "Przegląd zgłoszeń do tego sojuszu [#1].";
+$LOCA["pl"]["ALLY_APPA_NONE"] = "Brak oczekujących zgłoszeń.";
+
+?>
