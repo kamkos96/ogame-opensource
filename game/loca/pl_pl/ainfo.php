727ac41715bb4acdcd29ab84b57d1611fcf43cab
Kamil Kosiński <kamkos96@gmail.com>
2026-03-13 18:58:18 +0100

    Add Polish localization for alliance information texts

---
+++ game/loca/pl_pl/ainfo.php
@@ -0,0 +1,12 @@
+<?php
+
+// Lokalizacje dla zewnętrznej strony prezentacji sojuszu.
+
+$LOCA["pl"]["AINFO_INFO"] = "Informacje o sojuszu";
+$LOCA["pl"]["AINFO_TAG"] = "Tag:";
+$LOCA["pl"]["AINFO_NAME"] = "Nazwa:";
+$LOCA["pl"]["AINFO_MEMBERS"] = "Członkowie:";
+$LOCA["pl"]["AINFO_HOMEPAGE"] = "Strona domowa";
+$LOCA["pl"]["AINFO_PREV"] = "b.";
+
+?>
